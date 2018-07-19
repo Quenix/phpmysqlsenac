6938fa831e45fb9a0f2e19743588779c05c1058c
lokinhalp <41130457+lokinhalp@users.noreply.github.com>
2018-07-18 22:25:29 -0300

    .

--- Mariana/htdocs/index.php
+++ Mariana/htdocs/index.php
@@ -4,6 +4,11 @@
 		<title> PHP </title>
 	</head>
 	
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+	
+	<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.12.9/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
 	
 	<body>
 		
@@ -28,17 +33,60 @@
 			O POST ele faz uma requisição por trás
 			Normalmente pode acompanhar as requisições apertando F12, aba Network e carregando novamente a requisição.
 		-->	
-		<form method="POST" action="home.php">
-			<label> Email </label>
-			<input type="email" name="emailUsuario">
-			
-			<label> Senha </label>
-			<input type="password" name="senhaUsuario">	
-			
-			<button type="submit"> Enviar </button>
-		</form>
 		
-		<a href="cadastro.php">Faça seu Cadastro </a>
+		<nav class="navbar navbar-light bg-light">
+		
+			<!-- Button trigger modal -->
+			<button type="button" class="btn btn-primary" data-toggle="modal" data-target="#exampleModalCenter">
+			  Login
+			</button>
+
+			<!-- Modal -->
+			<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+			  <div class="modal-dialog modal-dialog-centered" role="document">
+				<div class="modal-content">
+				  <div class="modal-header">
+					<h5 class="modal-title" id="exampleModalLongTitle">Faça o Login</h5>
+					<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+					  <span aria-hidden="true">&times;</span>
+					</button>
+				  </div>
+				  <div class="modal-body">
+					<form method="POST" action="login.php">
+						<label> Email </label>
+						<input type="email" name="emailUsuario" >
+						
+						<label> Senha </label>
+						<input type="password" name="senhaUsuario" >	
+						
+						<div class="modal-footer">
+							<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+							<button type="submit" class="btn btn-primary">Logar</button>
+						</div>
+						<div class="modal-footer">
+							<a href="cadastro.php">Faça seu Cadastro </a>
+						</div>
+					</form>
+				  </div>
+				</div>
+			  </div>
+			</div>
+		</nav>
+		
+		<div class="content">
+		 
+		 <?php
+		 if (isset ($_GET['login'])){
+			 switch ($_GET['login']){
+				case 
+				 
+			echo '<div class="alert alert-danger" role="alert">
+					Usuário ou senha Inválidos
+				  </div>';
+		 }
+		 ?>
+		</div>
+		
 	
 	
 	</body>
