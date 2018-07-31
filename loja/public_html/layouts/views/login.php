d266bee4f2e06ed096f22c39211e0088a9db71d8
rvendramini <ricardinho.v@gmail.com>
2018-07-31 20:22:07 -0300

    asd

--- loja/public_html/layouts/views/login.php
+++ loja/public_html/layouts/views/login.php
@@ -3,19 +3,19 @@
 
         <form action="?page=home" method="POST">
             <div class="form-group">
-                <label for="exampleInputEmail1">Email address</label>
+                <label for="exampleInputEmail1">Endereço de E-mail</label>
                 <input type="email" name="email" class="form-control" id="exampleInputEmail1" aria-describedby="emailHelp" placeholder="Enter email">
-                <small id="emailHelp" class="form-text text-muted">We'll never share your email with anyone else.</small>
+                <small id="emailHelp" class="form-text text-muted">Nunca relate sua senha com estranhos.</small>
             </div>
             <div class="form-group">
-                <label for="exampleInputPassword1">Password</label>
+                <label for="exampleInputPassword1">Senha</label>
                 <input type="password" name="senha"  class="form-control" id="exampleInputPassword1" placeholder="Password">
             </div>
             <div class="form-check">
                 <input type="checkbox" class="form-check-input" id="exampleCheck1">
-                <label class="form-check-label" for="exampleCheck1">Check me out</label>
+                <label class="form-check-label" for="exampleCheck1">Mantenha conectado</label>
             </div>
-            <button type="submit" class="btn btn-primary">Submit</button>
+            <button type="submit" class="btn btn-primary">Entrar</button>
         </form>
     </div>
 
