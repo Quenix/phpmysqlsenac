903ed00e2d911601a4753c527adaadb62e3bac87
rvendramini <ricardinho.v@gmail.com>
2018-07-31 21:08:46 -0300

    logo.png
    add logo no navbar

--- loja/public_html/layouts/views/navbar.php
+++ loja/public_html/layouts/views/navbar.php
@@ -6,7 +6,8 @@
 
 ?>
 <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-        <a class="navbar-brand" href="#">Navbar</a>
+        <img id="logo" src="<?= baseUrlPublic('resources/logo.png') ?>" href=""></img>
+        
         <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
         </button>
