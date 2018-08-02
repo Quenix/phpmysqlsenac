89d9b31789ca940c906c7680c0991f96c46c62cf
lokinhalp <41130457+lokinhalp@users.noreply.github.com>
2018-08-01 21:09:50 -0300

    editar imagens

--- loja/app/produto/editarProduto.php
+++ loja/app/produto/editarProduto.php
@@ -1,4 +1,7 @@
 <?php
+move_uploaded_file($_FILES["foto_produto"]["tmp_name"], "resources/img/" . $_FILES["foto_produto"]["name"]);
+
+$foto_produto_dir =  baseUrlPublic("resources/img/") . $_FILES["foto_produto"]["name"];
 
 $query = "UPDATE produto SET nome_produto='$nome_produto', 
 preco_produto='$preco_produto', peso_produto='$peso_produto', detalhes_produto='$detalhes_produto', 
